4ac0dc4e210cd32c85772c397fb71b7a2a272bcc
Annes Huynh <76940774+alnhuynh@users.noreply.github.com>
2022-11-16 17:16:58 -0800

    Add files via upload

---
+++ src/Box.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Box {
+    private int x1, y1, x2, y2;
+    private String name;
+
+    public Box() {
+
+    }
+
+    public void create(Graphics g) {
+
+    }
+}
